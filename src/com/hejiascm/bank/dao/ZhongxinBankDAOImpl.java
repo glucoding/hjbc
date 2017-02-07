e67dce4385b82a754c69be7969650a5fcd4e035c
glutech <gehaol@qq.com>
2017-02-07 16:42:22 +0800

    Make sure the returnning xml translated back to object (GeneralBankResponse)

--- src/com/hejiascm/bank/dao/ZhongxinBankDAOImpl.java
+++ src/com/hejiascm/bank/dao/ZhongxinBankDAOImpl.java
@@ -11,7 +11,9 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+
 import javax.xml.namespace.QName;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Component;
@@ -19,6 +21,7 @@ import org.springframework.xml.transform.StringResult;
 import org.springframework.xml.transform.StringSource;
 
 import com.hejiascm.bank.domain.BankTransferRequest;
+import com.hejiascm.bank.domain.GeneralBankResponse;
 
 @Component("BankDAO")
 public class ZhongxinBankDAOImpl implements BankDAO {
@@ -37,13 +40,14 @@ public class ZhongxinBankDAOImpl implements BankDAO {
 		JAXBElement<BankTransferRequest> jaxbElement =  new JAXBElement( 
 	            new QName("stream"), BankTransferRequest.class, btr);
 		String xmlString = marshal(jaxbElement);
+		String result = null;
         
 		try {
 	        StringEntity entity = new StringEntity(xmlString);
 	        post.setEntity((org.apache.http.HttpEntity) entity);
 	        HttpResponse response;
 			response = client.execute(post);
-			String result = EntityUtils.toString(response.getEntity());
+			result = EntityUtils.toString(response.getEntity());
 		} catch (ClientProtocolException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -52,12 +56,19 @@ public class ZhongxinBankDAOImpl implements BankDAO {
 			e.printStackTrace();
 		}
         
+		result = "<?xml version='1.0' encoding='UTF-8'?>"
+				+ "<stream><status>good</status><statusText>goodEnough</statusText></stream>";
+		System.out.println(result);
+		GeneralBankResponse gbr = (GeneralBankResponse)unmarshal(result);
+
+		//GeneralBankResponse gbr = (GeneralBankResponse) jaxbElem.getValue();
+		System.out.println(gbr.getStatusText());
 		
 		return null;
 	}
 	
-	public JAXBElement<?> unmarshal(String xmlString) {
-		return (JAXBElement<?>) marshaller.unmarshal(new StringSource(xmlString));
+	public Object unmarshal(String xmlString) {
+		return marshaller.unmarshal(new StringSource(xmlString));
 	}
 	
 	public String marshal(JAXBElement<?> jaxbElement) {
